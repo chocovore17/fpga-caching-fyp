c27a353f731b3af0b537516ef3469169c39f22ee
chocovore17 <maelle.guerre@gmail.com>
2022-03-10 10:28:08 +0000

    top level works, very basic to improve

---
+++ models/clockedmemory/questasim_run_top.sh
@@ -0,0 +1,33 @@
+# basic bash script to run vlog compilation and vsim simulation in Questa Sim
+# source /usr/local/mentor/QUESTA-CORE-PRIME_10.7c/settings.sh
+
+# run ./questasim_run.sh without argument to normal simulation
+# run ./questasim_run.sh -c (with argument) for coverage report
+vlib work
+
+if [  -z $1 ] 
+then 
+vlog -work work +acc=blnr -noincr -timescale 1ns/1ps testbench/unit_level/UPDOWNSTREAM/tb_top_UPDOWNSTREAM.sv shared/top.sv
+vopt -work work tb_top_UPDOWNSTREAM  -o seg_work_opt
+vsim seg_work_opt -c -logfile testbench/outputs/top/UPDOWNSTREAM_sim.txt  -do "run -all; quit"
+
+
+else 
+# run converage check on questasim
+rm -r covhtmlreport/
+rm testbench/outputs/top/UPDOWNSTREAM_cov_report.txt
+vlog -work work +acc=blnr -noincr -timescale 1ns/1ps testbench/unit_level/UPDOWNSTREAM/tb_top_UPDOWNSTREAM.sv shared/top.sv
+vopt -work work tb_top_UPDOWNSTREAM  -o seg_work_opt
+vsim -coverage seg_work_opt -c -logfile testbench/outputs/top/UPDOWNSTREAM_sim.txt  -do "run -all; coverage report -file testbench/outputs/top/UPDOWNSTREAM_cov_report.txt -byfile -assert -directive -cvg -codeAll; coverage save -onexit -assert -directive -cvg -codeAll testbench/outputs/upstream/upstream.ucdb; quit"
+
+# vsim -coverage seg_work_opt -gui 
+
+vcover report -details -html testbench/outputs/top/updown.ucdb
+
+# firefox covhtmlreport/index.html
+fi
+
+# Command for top-level testing
+# vlog -work work +acc=blnr -noincr -timescale 1ns/1ps  -f testbench/ahblite_sys.vc
+# vopt -work work ahblite_sys_tb -o top_work_opt
+# vsim top_work_opt -gui -do "log -r /*; add wave *; run 1500ns" 
---
+++ testbench/unit_level/UPDOWNSTREAM/monitor_UPDOWNSTREAM.sv
@@ -0,0 +1,42 @@
+`define MON_IF updown_if.monitor_cb
+class monitor;
+    virtual updownstream_if.MONITOR updown_if; //interface modport in driver class
+    mailbox mon2scb; // mailbox to hold package/transaction from monitor to scoreboard
+    event mon_done;// event to synchronise monitor and scoreboard
+
+    //constructor
+    function new(virtual updownstream_if.MONITOR updown_if, mailbox mon2scb);
+        //getting the interface
+        this.updown_if = updown_if;
+        //getting the mailbox handles from  environment 
+        this.mon2scb = mon2scb;
+    endfunction    
+
+    task main;
+        $display("T=%0t, [MONITOR] starting ---------",$time);
+        forever begin
+             @(posedge updown_if.slowclk);
+             if(1'b1==1'b1)begin //valid slave select signal
+                // I/O transactions 
+                transaction trans = new();
+                // general inputs
+                trans.cpu_new_max = `MON_IF.cpu_new_max;
+                trans.cpu_go = `MON_IF.cpu_go;
+                trans.cpu_amount = `MON_IF.cpu_amount;
+                trans.cpu_client_id = `MON_IF.cpu_client_id;
+                trans.exchange_client_id = `MON_IF.exchange_client_id;
+                trans.exchange_amount = `MON_IF.exchange_amount;
+                trans.exchange_go = `MON_IF.exchange_go;
+                // outputs
+                trans.accumulated_orders = `MON_IF.accumulated_orders;
+                trans.cancelled_orders = `MON_IF.cancelled_orders;
+
+                @(posedge updown_if.slowclk);
+                // trans.print("MONITOR");
+                mon2scb.put(trans);
+                -> mon_done;
+             end
+        end
+    endtask
+
+endclass
